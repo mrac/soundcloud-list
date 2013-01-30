01130e227310533ebc1e8e23054348a63dce512e
Michal Raczkowski <michal.raczkowski@gmail.com>
2013-01-30 01:44:03 +0100

    added SC sdk initialization

--- app/router.js
+++ app/router.js
@@ -11,8 +11,19 @@ function(app) {
       "": "index"
     },
 
+    initialize: function() {
+      // Initialize SoundCloud SDK
+      this.initSoundCloud();
+    },
+
     index: function() {
 
+    },
+
+    initSoundCloud: function() {
+      SC.initialize({
+        client_id: "6d6kei19bmrac"
+      });
     }
   });
 
