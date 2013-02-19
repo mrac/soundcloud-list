46f88f0b90acfa458641cdcff39d95f37e68f101
Michal Raczkowski <michal.raczkowski@gmail.com>
2013-02-19 17:28:37 +0100

    added custom shim

---
+++ vendor/js/libs/custom-shim.js
@@ -0,0 +1,22 @@
+// Copyright 2009-2012 by contributors, MIT License
+// vim: ts=4 sts=4 sw=4 expandtab
+
+// Module systems magic dance
+(function (definition) {
+    // RequireJS
+    if (typeof define == "function") {
+        define(definition);
+    // YUI3
+    } else if (typeof YUI == "function") {
+        YUI.add("es5", definition);
+    // CommonJS and <script>
+    } else {
+        definition();
+    }
+})(function () {
+
+    if(typeof console === "undefined") {
+        window.console = {log: function() {}, error: function() {}};
+    }
+
+});
